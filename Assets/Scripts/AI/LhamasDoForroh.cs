b501c9713bb40d0fa49224c28cc4f045773ade0a
Henrique Ribeiro Nassi <41037970+hrn1999@users.noreply.github.com>
2019-10-16 14:20:58 -0300

    Atualizacao do LhamasDoForroh

--- Assets/Scripts/AI/LhamasDoForroh.cs
+++ Assets/Scripts/AI/LhamasDoForroh.cs
@@ -1,120 +1,135 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Panda;
 using UnityEngine;
 using UnityEngine.AI;
+using Panda;
 
-public class LhamasDoForroh : MonoBehaviour {
-    [SerializeField]
-    private TankAI m_Tank;
-    private float minDistance;
-    NavMeshHit hit;
-    private void Awake () {
-        m_Tank = GetComponent<TankAI> ();
+public class LhamasDoForroh : MonoBehaviour
+{
+    private TankAI _tank;
+    private float _minDistance;
+
+    private void Awake()
+    {
+        _tank = GetComponent<TankAI>();
     }
 
     [Task]
-    public void PickRandomDestination () {
-        Vector3 destination = new Vector3 (Random.Range (-20.0f, 20.0f), 0.0f, Random.Range (-20.0f, 20.0f));
+    public void PickRandomDestination()
+    {
         Vector3 randomDirection = Random.insideUnitSphere * 20.0f;
-        randomDirection += transform.position;
-        NavMesh.SamplePosition (randomDirection, out hit, 20.0f, NavMesh.AllAreas);
-        Vector3 finalPosition = hit.position;
-        m_Tank.Agent.SetDestination (destination);
-        Task.current.Succeed ();
+        
+        //NavmeshHit
+        NavMeshHit navMeshHit;
+        NavMesh.SamplePosition(randomDirection, out navMeshHit, 20.0f, NavMesh.AllAreas);
+        
+        Vector3 destination = navMeshHit.position;
+        
+        _tank.Agent.SetDestination(destination);
+        Task.current.Succeed();
     }
 
     [Task]
-    public void MoveDestination () {
-        Vector3 myPosition = m_Tank.transform.position;
-        Vector3 rayDirection = m_Tank.transform.forward;
-        int rayLengthMeters = 3;
-        RaycastHit hitInfo;
-
-        if (Physics.Raycast (myPosition, rayDirection, out hitInfo, rayLengthMeters)) {
-            Task.current.Fail ();
+    public void MoveDestination()
+    {
+        //Raycast para frente do tanque.
+        RaycastHit hit;
+        if (Physics.Raycast(_tank.Position, _tank.transform.forward, out hit, 3f))
+        {
+            Task.current.Fail();
         }
 
-        if (m_Tank.Agent.remainingDistance <= m_Tank.Agent.stoppingDistance && !m_Tank.Agent.pathPending) {
-            Task.current.Succeed ();
-        }
-    }
-
-    [Task]
-    public bool SeeEnemy () {
-        bool retorno = false;
-        if (m_Tank.Targets.Count > 0) {
-            for (int i = 0; i < m_Tank.Targets.Count; i++) {
-                if (m_Tank.Targets[i] != null) {
-                    retorno = true;
-                }
-            }
-        }
-        return retorno;
-    }
-
-    [Task]
-    public bool InDanger (float minDistance) {
-        return Vector3.Distance (m_Tank.Targets[0], transform.position) < minDistance;
-    }
-
-    [Task]
-    public bool DistanceToShoot (float minDistance) {
-        return Vector3.Distance (m_Tank.Targets[0], transform.position) > minDistance;
-    }
-
-    [Task]
-    public void GetDistance () {
-        if(!DistanceToShoot(15.0f)){
-            m_Tank.Move (-1.0f);
-        }
-        Task.current.Succeed ();
-    }
-
-    [Task]
-    public void TakeCover () {
-        m_Tank.StopFire ();
-        Vector3 awayFromTarget = (transform.position - m_Tank.Targets[0]).normalized;
-        Vector3 destination = transform.position + awayFromTarget * 5;
-        m_Tank.Agent.SetDestination (destination);
-        Task.current.Succeed ();
-    }
-
-    [Task]
-    public bool IsHealthLessThan (float health) {
-        return m_Tank.Health < health;
-    }
-
-    [Task]
-    public void AlignTank () {
-        if(!ShootLinedUp()){
-            m_Tank.LookAt (m_Tank.Targets[0]);
-        }else{
+        if (_tank.Agent.remainingDistance <= _tank.Agent.stoppingDistance && !_tank.Agent.pathPending)
+        {
             Task.current.Succeed();
         }
     }
 
     [Task]
-    public bool ShootLinedUp () {
-        Vector3 dir = transform.position - m_Tank.Targets[0];
-        float angle=Vector3.Angle(transform.position, dir);
+    public bool SeeEnemy()
+    {
+        return _tank.HasTargetInRange;
+    }
+
+    [Task]
+    public bool InDanger(float _minDistance)
+    {
+        return _tank.DistanceToTarget(_tank.Targets[0]) < _minDistance;
+    }
+
+    [Task]
+    public bool DistanceToShoot(float _minDistance)
+    {
+        return _tank.DistanceToTarget(_tank.Targets[0]) > _minDistance;
+    }
+
+    [Task]
+    public void GetDistance()
+    {
+        if (!DistanceToShoot(10.0f))
+        {
+            PickRandomDestination();
+        }
+        //Raycast para trás do tanque.
+        RaycastHit hit;
+        if (Physics.Raycast(_tank.Position, -_tank.transform.forward, out hit, 1.0f))
+        {
+            Task.current.Fail();
+        }
+        
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public void TakeCover()
+    {
+        _tank.StopFire();
+        Vector3 awayFromTarget = (_tank.Position - _tank.Targets[0]).normalized;
+        Vector3 destination = _tank.Position + awayFromTarget * 5;
+        _tank.Agent.SetDestination(-destination);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public bool IsHealthLessThan(float health)
+    {
+        return _tank.Health < health;
+    }
+
+    [Task]
+    public void AlignTank()
+    {
+        _tank.LookAt(_tank.Targets[0]);
+        if (ShootLinedUp())
+            Task.current.Succeed();
+    }
+
+    [Task]
+    public bool ShootLinedUp()
+    {
+        float angle = _tank.Angle(_tank.Targets[0]);
         Debug.Log(angle);
-        if (Mathf.Abs(angle)<5) {
+        if (Mathf.Abs(angle) <= 5)
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
 
     [Task]
-    public void Fire () {
-        m_Tank.StartFire ();
-        Task.current.Succeed ();
+    public void Fire()
+    {
+        _tank.StartFire();
+        Task.current.Succeed();
     }
 
     [Task]
-    public void StopFire () {
-        m_Tank.StopFire ();
-        Task.current.Succeed ();
+    public void StopFire()
+    {
+        _tank.StopFire();
+        Task.current.Succeed();
     }
 }
