15613cfe508ccd681f86718cec35a0b9a1fde062
Henrique Ribeiro Nassi <41037970+hrn1999@users.noreply.github.com>
2019-10-17 12:14:16 -0300

    Rotação Arrumada? Acho que sim

--- Assets/Scripts/AI/LhamasDoForroh.cs
+++ Assets/Scripts/AI/LhamasDoForroh.cs
@@ -8,7 +8,6 @@ public class LhamasDoForroh : MonoBehaviour
 {
     private TankAI _tank;
     private float _minDistance;
-    Vector3 destination;
 
     private void Awake()
     {
@@ -18,35 +17,99 @@ public class LhamasDoForroh : MonoBehaviour
     [Task]
     public void PickRandomDestination()
     {
+        //Clear Current Path
         _tank.Agent.ResetPath();
+
         Vector3 randomDirection = Random.insideUnitSphere * 20.0f;
+        Vector3 destination = Vector3.zero;
 
         //NavmeshHit
         NavMeshHit navMeshHit;
         NavMesh.SamplePosition(randomDirection, out navMeshHit, 20.0f, NavMesh.AllAreas);
 
+        //Set Destination
         destination = navMeshHit.position;
-
         _tank.Agent.SetDestination(destination);
-        
+
+        //Debug
+        Debug.DrawLine(_tank.Position, destination, Color.blue);
+
         Task.current.Succeed();
     }
 
     [Task]
     public void MoveDestination()
     {
-        //Raycast para frente do tanque.
-        
-
-        RaycastHit hit;
-        if (Physics.Raycast(_tank.Position, _tank.transform.forward, out hit, 3f))
-        {
-            Task.current.Fail();
-        }
-
+        //Arrived at destinantion
         if (_tank.Agent.remainingDistance <= _tank.Agent.stoppingDistance && !_tank.Agent.pathPending)
         {
             Task.current.Succeed();
+        }
+    }
+
+
+    [Task]
+    public void GetDistance()
+    {
+        if (!DistanceToShoot(10.0f))
+        {
+            // _tank.Agent.isStopped = true;
+            // Vector3 disToEnemy = _tank.Targets[0] - _tank.Position;
+            // Vector3 targetPos = disToEnemy.normalized * -3.0f;
+            // _tank.Agent.SetDestination(targetPos);
+            Fire();
+        }
+
+        _tank.Agent.isStopped = false;
+        _tank.Agent.ResetPath();
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public void TakeCover()
+    {
+        _tank.StopFire();
+        Vector3 awayFromTarget = (_tank.Position - _tank.Targets[0]).normalized;
+        Vector3 destination = _tank.Position + awayFromTarget * 5;
+        _tank.Agent.SetDestination(-destination);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public void Fire()
+    {
+        _tank.StartFire();
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public void StopFire()
+    {
+        _tank.StopFire();
+        Task.current.Succeed();
+    }
+
+    private void Update()
+    {
+        if (HasTargetInRange())
+        {
+            var lookRotation = Quaternion.LookRotation((_tank.Targets[0] - _tank.Position).normalized);
+            _tank.transform.rotation = Quaternion.Slerp(_tank.transform.rotation, lookRotation, Time.deltaTime * 2.0f);
+        }
+    }
+
+    [Task]
+    public bool ShootLinedUp()
+    {
+        float angle = _tank.Angle(_tank.Targets[0]);
+        Debug.Log(angle);
+        if (Mathf.Abs(angle) <= 10)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
@@ -69,87 +132,8 @@ public class LhamasDoForroh : MonoBehaviour
     }
 
     [Task]
-    public void GetDistance()
-    {
-        // _tank.Agent.Stop();
-        // Vector3 distToEnemy = _tank.Targets[0] - _tank.Position;
-        // Vector3 targetPos = distToEnemy.normalized * -3.0f;
-        // _tank.Agent.destination = targetPos;
-        // _tank.Agent.Resume();
-
-
-
-        if (!DistanceToShoot(15.0f))
-        {
-            _tank.Agent.Stop();
-            _tank.Agent.ResetPath();
-            //_tank.Move(-1f);
-        }
-
-        //Raycast para trás do tanque.
-        // RaycastHit hit;
-        // if (Physics.Raycast(_tank.Position, -_tank.transform.forward, out hit, 1.0f))
-        // {
-        //     Task.current.Fail();
-        // }
-
-        Task.current.Succeed();
-    }
-
-    [Task]
-    public void TakeCover()
-    {
-        _tank.StopFire();
-        Vector3 awayFromTarget = (_tank.Position - _tank.Targets[0]).normalized;
-        Vector3 destination = _tank.Position + awayFromTarget * 5;
-        _tank.Agent.SetDestination(-destination);
-        Task.current.Succeed();
-    }
-
-    [Task]
     public bool IsHealthLessThan(float health)
     {
         return _tank.Health < health;
-    }
-
-    [Task]
-    public bool ShootLinedUp()
-    {
-        float angle = _tank.Angle(_tank.Targets[0]);
-        Debug.Log(angle);
-        if (Mathf.Abs(angle) <= 10)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    [Task]
-    public void Fire()
-    {
-        _tank.StartFire();
-        Task.current.Succeed();
-    }
-
-    [Task]
-    public void StopFire()
-    {
-        _tank.StopFire();
-        Task.current.Succeed();
-    }
-
-    private void Update()
-    {
-        if (HasTargetInRange())
-        {
-            //_tank.Agent.Stop();
-            //_tank.Agent.ResetPath();
-            _tank.LookAt(_tank.Targets[0]);
-        }
-
-        Debug.DrawLine(_tank.Position, destination, Color.blue);
     }
 }
